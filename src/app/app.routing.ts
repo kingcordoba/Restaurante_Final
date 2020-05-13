aec793809e110500f0a3b9f121c605f7f02b7c43
Anthony Urrego <anthourrego@gmail.com>
2020-05-12 23:31:56 -0500

    Actualización segunda parte

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,15 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
 import { LandingComponent } from './examples/landing/landing.component';
-import { LoginComponent } from './examples/login/login.component';
 import { ProfileComponent } from './examples/profile/profile.component';
+
 import { InicioComponent } from './web/inicio/inicio.component';
+import { LoginComponent } from './web/login/login.component';
 
 const routes: Routes =[
     { path: '', component: InicioComponent},
     { path: 'index',                component: InicioComponent },
+    { path: 'login',       component: LoginComponent },
     { path: 'examples/landing',     component: LandingComponent },
-    { path: 'examples/login',       component: LoginComponent },
     { path: 'examples/profile',     component: ProfileComponent }
 ];
 
