2040ced19ae52bdba623982a8fee2dc2f574a904
Anthony Urrego <anthourrego@gmail.com>
2020-05-18 23:17:18 -0500

    Subida de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,15 @@
 
 export const environment = {
   production: false,
-  urlAPi: 'http://192.168.0.3:8000/api/'
+  urlAPi: 'http://192.168.0.3:8000/api/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBTbGsnNwhzP-_jC0ozSdTC1WpsOMmGKNI",
+    authDomain: "restaurantetato-7257a.firebaseapp.com",
+    databaseURL: "https://restaurantetato-7257a.firebaseio.com",
+    projectId: "restaurantetato-7257a",
+    storageBucket: "restaurantetato-7257a.appspot.com",
+    messagingSenderId: "942286705366",
+    appId: "1:942286705366:web:bbeac753b7c7d0a644ef77",
+    measurementId: "G-Q2SF51CDGF"
+  }
 };
